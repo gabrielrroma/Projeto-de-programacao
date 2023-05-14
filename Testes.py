a9e3457a72b46d0054a1c18e7210a2ef233eace0
Gabriel Roma <gabrielrroma@gmail.com>
2023-05-14 05:41:47 -0300

    Update Testes.py

--- Testes.py
+++ Testes.py
@@ -2,17 +2,22 @@ from lib.interface import *
 from lib.manipulador import *
 from time import sleep
 
+#Definindo em uma variável qual será o arquivo para guardar transações
 txt = 'saldo.txt'
 
 if not arquivoExiste(txt):
     criarArquivo(txt)
 
-
+#Menu para fazer operações
 while True:
+    #Função menu() para adicionar opções de escolha
     opcao = menu(['Sair','Adicionar transação','Listar transações','Listar transações por categoria'])
+#Condição para opção desejada
+    #Codição 0: Para fechar o programa
     if opcao == 0:
         print(arrumador('Fechando sistema!'))
         break
+    #Condição 1: Para adicionar transações
     elif opcao == 1:
         nome = input("Digite o nome da transação: ")
         categorias = input("Categoria da transação((C)Casa, (T)Transporte, (S)Saúde, (L)Lazer, (A)Alimentação): ").upper()
@@ -37,10 +42,12 @@ while True:
                 continue
         valor = input("Digite o valor da transação: ")    
         criarArquivo(txt,nome,categoria,valor)
+    #Condição 2: Para listar todas as transações    
     elif opcao == 2:
         leitorArquivo(txt,'Lista de Transações')
+    #Condição 3: Para listar transações por categoria    
     elif opcao == 3:
-        print('3')     
+        arquivoCategoria(txt,'Lista de Transações por categoria')     
     else:
         print('Tente uma opção válida.')
     sleep(1)    
